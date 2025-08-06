3fd0cebf9c3911698de5b9da2caa68c79d54d7fc
Andre Menezes <djdedemenezes@gmail.com>
2025-08-06 17:43:39 -0300

    playing wtih tailwind basics

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup></script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank"
+  <div class="container mx-auto max-w-9xl bg-green-100">
+    <div class="flex w-full gap-[1rem] justify-center mb-3">
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank"
       ><img src="./assets/vue.svg" class="logo vue" alt="Vue logo"
-    /></a>
+      /></a>
+    </div>
+    <h1 class="text-violet-900 text-center">Welcome</h1>
+
   </div>
-  <h1 class="text-violet-900">Welcome</h1>
 </template>
 
 <style scoped></style>
