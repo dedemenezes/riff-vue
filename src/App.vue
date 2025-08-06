bfa874199e0b7011ed45249c0e2fa200cc2fbd85
Andre Menezes <djdedemenezes@gmail.com>
2025-08-06 17:04:24 -0300

    setup tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
+  <h1 class="text-violet-900">Welcome</h1>
 </template>
 
 <style scoped>
