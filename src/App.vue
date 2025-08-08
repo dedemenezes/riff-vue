bcdac91cb4c5a555f5fe43d0d7b817bc08a2bae2
Andre Menezes <djdedemenezes@gmail.com>
2025-08-08 14:20:39 -0300

    Implement tailwind color class names using json color tokens

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
-<script setup></script>
+<script setup>
+  import ListCard from "./components/ListCard.vue";
+</script>
 
 <template>
   <div class="container mx-auto max-w-9xl bg-green-100">
-    <div class="flex w-full gap-[1rem] justify-center mb-3">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank"
-      ><img src="./assets/vue.svg" class="logo vue" alt="Vue logo"
-      /></a>
-    </div>
     <h1 class="text-violet-900 text-center">Welcome</h1>
-
+    <ListCard />
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
+import * as colorPrimitives from "./design-tokens/color-primitives.json";
+import * as colorTokens from "./design-tokens/color-tokens.json";
+
+const isPrimitive = (tokenValue) => {
+  return typeof tokenValue === 'string' && tokenValue.startsWith('{')
+}
+
+// Simple resolver that replaces "{neutrals.100}" with "#FFFFFF"
+function resolveRefs(jsonTokens, jsonPrimitives) {
+  const result = {};
+  for (const token in jsonTokens) {
+    const tokenValue = jsonTokens[token];
+    if (isPrimitive(tokenValue)) {
+      const colorAndShade = tokenValue.replace(/[{}]/g, '').split('.');
+      const hexColor = colorAndShade.reduce((acc, part) => acc[part], jsonPrimitives);
+      result[token] = hexColor;
+    } else if (typeof tokenValue === 'object') {
+      result[token] = resolveRefs(tokenValue, jsonPrimitives);
+    }
+  }
+  return result;
+}
+
 export default {
   content: ["./src/**/*.{vue,js,ts}"],
   theme: {
     extend: {
-      colors: {
-        "laranja": {
-          200: '#FFE1C3',
-          400: '#FFB46A',
-          600: '#FF7F00',
-          800: '#C06103',
-          1000: '#8A4805'
-        },
-        "magenta": {
-          200: '#FFD4E9',
-          400: '#FF6FB7',
-          600: '#FF007F',
-          800: '#B20059',
-          1000: '#6A0035'
-        },
-        "vermelho": {
-          200: '#FFCCCC',
-          400: '#FF7E7E',
-          600: '#FF0000',
-          800: '#A30000',
-          1000: '#5E0000'
-        },
-        "violeta": {
-          200: '#DA9FE0',
-          400: '#BF68C8',
-          600: '#7F008C',
-          800: '#580161',
-          1000: '#2D0032'
-        },
-        "amarelo": {
-          200: '#FFF8DF',
-          400: '#FFECAB',
-          600: '#FFDB5C',
-          800: '#FFC700',
-          1000: '#D4A500'
-        },
-        "verde": {
-          200: '#CAFFCF',
-          400: '#9EF4A6',
-          600: '#1BBC2B',
-          800: '#11861D',
-          1000: '#094A0F'
-        },
-      }
+      colors: resolveRefs(colorTokens, colorPrimitives)
     }
   },
   plugins: [],
