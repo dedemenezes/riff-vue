dd09480f56ef0152d3ae9bbfd3bcfd47234c728d
Andre Menezes <djdedemenezes@gmail.com>
2025-08-07 23:16:11 -0300

    display list with list card component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import ListCard from "./components/ListCard.vue";
+import { fetchMovies } from "./services/api/movies.js";
+
+const movies = ref(null);
+
+onMounted(async () => {
+  if (!movies.value) {
+    const data = await fetchMovies();
+    if (data) movies.value = data.results;
+  }
+});
+</script>
 
 <template>
   <div class="container mx-auto max-w-9xl bg-green-100">
-    <div class="flex w-full gap-[1rem] justify-center mb-3">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank"
-      ><img src="./assets/vue.svg" class="logo vue" alt="Vue logo"
-      /></a>
-    </div>
     <h1 class="text-violet-900 text-center">Welcome</h1>
-
+    <div class="p-3 grid grid-cols-3 gap-4 mx-auto">
+      <ListCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+    </div>
   </div>
 </template>
 
---
+++ src/components/ListCard.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  movie: Object,
+});
+
+const posterUrl = computed(() => {
+  return `https://image.tmdb.org/t/p/w500${props.movie.poster_path}`;
+});
+
+const backgroundStyle = computed(() => ({
+  backgroundImage: `
+    linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.00) 49.89%,
+      rgba(0, 0, 0, 0.50) 71.29%,
+      rgba(0, 0, 0, 0.76) 86.95%
+    ),
+    url(${posterUrl.value})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+}));
+</script>
+<template>
+  <!-- max-w-[332px]  -->
+  <div
+    :style="backgroundStyle"
+    class="flex flex-col justify-end gap-[10px] px-[24px] py-[16px] rounded-lg bg-violet-200 h-[455px]"
+  >
+    <p
+      class="text-[#FFFFFF] text-[10px] font-medium tracking-[0.09375em] uppercase"
+    >
+      {{ props.movie.original_title }}
+    </p>
+    <p class="text-white text-sm font-normal leading-[21px]">
+      {{ props.movie.overview }}
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
