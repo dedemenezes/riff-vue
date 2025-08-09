0b815a4e2d4586a307e4eae6d0f797c7de59b129
Andre Menezes <djdedemenezes@gmail.com>
2025-08-09 00:28:26 -0300

    add sponsor header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import ListCard from "./components/ListCard.vue";
 import { fetchMovies } from "./services/api/movies.js";
+import SponsorHeader from "./components/SponsorHeader.vue";
 
 const movies = ref(null);
 
@@ -14,24 +14,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="container mx-auto max-w-9xl bg-verde-200 hover:bg-verde-600 transition-colors">
-    <h1 class="font-heading font-regular text-text-danger-tertiary text-center text-3xl">Welcome</h1>
-    <!-- <ListCard /> -->
-     <button class="-m-150 font-body py-100 px-150 bg-laranja-600 border-4 border-magenta-800 text-neutrals-100 rounded focus:outline ring-amarelo-800 focus:ring-2 focus:ring-offset-2">RioMarket</button>
-  </div>
-  <button class="px-200 py-150 bg-amarelo-200 blur-sm rounded-200 focus:outline focus:outline-offset-4 focus:outline-verde-200 ...">Button A</button>
-
-  <div class="container mx-auto max-w-9xl  bg-verde-200 hover:bg-verde-600 transition-colors">
-    <h2 class="font-heading font-regular text-vermelho-200 text-center text-3xl">Welcome</h2>
-
-    <div class="p-3 grid grid-cols-3 gap-4 mx-auto">
-      <ListCard v-for="movie in movies" :key="movie.id" :movie="movie" />
-    </div>
-  </div>
-
-  <button class="px-200 py-150 bg-amarelo-200 blur-sm rounded-200 focus:outline focus:outline-offset-4 focus:outline-verde-200 ...">Button A</button>
+  <SponsorHeader class="bg-azul-400" />
 </template>
 
-<!-- <template> -->
-<!-- </template> -->
 <style scoped></style>
---
+++ src/components/SponsorHeader.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="font-body flex items-center justify-center h-[72px] py-300 px-4000">
+    <p class="text-md uppercase leading-[22.4px]"><span class="font-semibold">MINISTÉRIO DA CULTURA</span>, <span class="font-semibold">SHELL</span> e <span class="font-semibold">PREFEITURA DO RIO</span> apresentam</p>
+  </div>
+</template>
+
+<style scoped></style>
