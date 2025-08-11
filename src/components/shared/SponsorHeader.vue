3dabdb9952ada98df4a7ae255cf1ee8065b39c07
Andre Menezes <djdedemenezes@gmail.com>
2025-08-11 11:00:36 -0300

    fix sponsor header responsiveness

--- src/components/shared/SponsorHeader.vue
+++ src/components/shared/SponsorHeader.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <div class="font-body flex items-center justify-center h-[72px] py-300 px-4000">
-    <p class="text-md uppercase leading-[22.4px]"><span class="font-semibold">MINISTÉRIO DA CULTURA</span>, <span class="font-semibold">SHELL</span> e <span class="font-semibold">PREFEITURA DO RIO</span> apresentam</p>
+  <div class="font-body flex items-center justify-center h-[72px] p-300 md:py-300 md:px-4000">
+    <p class="text-sm md:text-md uppercase leading-[22.4px] text-center"><span class="font-semibold">MINISTÉRIO DA CULTURA</span>, <span class="font-semibold">SHELL</span> e <span class="font-semibold">PREFEITURA DO RIO</span> apresentam</p>
   </div>
 </template>
 
