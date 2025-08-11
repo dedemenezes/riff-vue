6b019090a7b5757e97b3c7f7b1853cc88a8b9ffe
Andre Menezes <djdedemenezes@gmail.com>
2025-08-11 13:02:29 -0300

    reorganize class binding

--- src/components/buttons/BaseButton.vue
+++ src/components/buttons/BaseButton.vue
@@ -13,7 +13,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <button class="flex items-center justify-center gap-2 px-3 py-2 rounded-lg text-sm font-semibold leading-[19.6px] transition-colors duration-200" :class="colors">
+  <button class="flex items-center justify-center gap-2 px-3 py-2 rounded-lg text-sm font-semibold leading-[19.6px] transition-colors duration-200"
+          :class="colors">
     {{ props.text }}
     <img src="../../assets/Vector.svg" alt="carret" class="w-full">
   </button>
