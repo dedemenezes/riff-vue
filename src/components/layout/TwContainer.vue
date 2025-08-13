bc18df53b1de273c3ca6c15f24174c5d94793313
Andre Menezes <djdedemenezes@gmail.com>
2025-08-13 11:42:22 -0300

    add layout container component

---
+++ src/components/layout/TwContainer.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { extraClasses } = defineProps({
+  extraClasses: {
+    type: String,
+    default: ""
+  }
+})
+</script>
+<template>
+  <div class="w-full px-3 mx-auto lg:max-w-4xl xl:max-w-7xl xxl:max-w-xxl"
+    :class="extraClasses"
+  >
+    <slot />
+  </div>
+</template>
