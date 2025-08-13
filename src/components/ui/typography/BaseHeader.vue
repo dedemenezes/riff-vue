abfcd70499f65899fe575142f34551951284ceff
Andre Menezes <djdedemenezes@gmail.com>
2025-08-13 11:43:05 -0300

    refac heading component

---
+++ src/components/ui/typography/BaseHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+const { fontSize } = defineProps({
+  fontSize: {
+    type: String,
+    default: 'text-xl'
+  }
+})
+
+const baseClasses = "text-neutrals-1000 font-heading font-semibold  leading-[120%]"
+
+</script>
+
+<template>
+  <h1
+    :class="[baseClasses, fontSize]"
+  ><slot /></h1>
+</template>
+
+<style scoped>
+</style>
