b9f8d292f1918d5730ac8ebfd9cea00e35a6dba2
Andre Menezes <djdedemenezes@gmail.com>
2025-08-13 10:41:01 -0300

    add typography components

---
+++ src/components/ui/typography/HeadingLarge.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { title } = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <h1
+    class="text-neutrals-900 font-heading font-semibold text-2xl leading-[38.4px]"
+  >{{ title }}</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ui/typography/SubHeading.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { content } = defineProps({
+  content: { type: String, required: true }
+})
+</script>
+
+<template>
+  <p
+    class="text-neutrals-900 font-body text-xl font-regular leading-[30px]"
+  >{{ content }}</p>
+</template>
+
+<style scoped>
+</style>
