098d67133756841f7d202092c26ffeafcbf92034
Andre Menezes <djdedemenezes@gmail.com>
2025-08-19 00:54:07 -0300

    token not needed

--- src/services/api/api_client.js
+++ src/services/api/api_client.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: { Accept: 'application/json'}
+  headers: { Accept: 'text/xml'}
 })
 
-apiClient.interceptors.request.use((config) => {
-  const token = import.meta.env.VITE_TMDB_TOKEN
-  if(token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-})
+// apiClient.interceptors.request.use((config) => {
+//   const token = import.meta.env.VITE_TMDB_TOKEN
+//   if(token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
 
 apiClient.interceptors.response.use(
   (response) => response,
