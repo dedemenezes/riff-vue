6e137842fcd9a67085fea60dc4d35a609bd7e758
Andre Menezes <djdedemenezes@gmail.com>
2025-08-21 00:27:55 -0300

    add remove functionality via close icon

--- src/components/ui/tags/TagFilter.vue
+++ src/components/ui/tags/TagFilter.vue
@@ -6,13 +6,18 @@ const { text } = defineProps({
     required: true,
   },
 });
+
+const emit = defineEmits(['remove-filter'])
+const removeSelf = (event) => {
+  console.log(event);
+  emit("remove-filter", text)
+}
 </script>
 
 <template>
   <span
     class="max-w-fit inline-flex items-center gap-100 px-200 py-100 border rounded-full border-neutrals-300 font-body text-xs text-neutrals-700 font-regular leading-[18px]"
-    >{{ text }} <IconClose
-  /></span>
+  >{{ text }} <IconClose class="cursor-pointer" @click="removeSelf"/></span>
 </template>
 
 <style scoped></style>
