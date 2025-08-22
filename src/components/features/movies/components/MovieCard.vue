f92701330a3fecb4fcb7efaeab8c6b007226dda6
Andre Menezes <djdedemenezes@gmail.com>
2025-08-22 17:57:48 -0300

    add image default from public

--- src/components/features/movies/components/MovieCard.vue
+++ src/components/features/movies/components/MovieCard.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 });
 
 const moviePoster = computed(() => {
-  return props.movie.poster || "src/assets/poc-poster.jpg"
+  return props.movie.poster || "/public/poc-poster.jpg"
 })
 
 const movieGenre = computed(() => {
