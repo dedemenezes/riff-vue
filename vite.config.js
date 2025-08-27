06d90946ed77da76c4a5a3490ceca21feb3f9b89
Andre Menezes <djdedemenezes@gmail.com>
2025-08-27 20:02:01 -0300

    add jsdom and vue-utils for testing

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
+  test: {
+    environment: "jsdom", // <-- important
+    globals: true,
+  },
   server: {
     allowedHosts: ["cd216d0f94e7.ngrok-free.app"],
   },
