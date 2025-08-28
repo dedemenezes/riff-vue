c3c261c2faa5ac995f747d9d922ae9e5f3a772a6
Andre Menezes <djdedemenezes@gmail.com>
2025-08-27 21:59:55 -0300

    add navbar-dropdown panel

---
+++ src/components/layout/navbar/DropdownPanel.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  isOpen: Boolean,
+  onMouseEnter: Function,
+  onMouseLeave: Function
+});
+</script>
+
+<template>
+  <transition
+    enter-active-class="transition-opacity duration-400 ease-in-out"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition-opacity duration-400 ease-in-out"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div
+      v-show="props.isOpen"
+      @mouseenter="props.onMouseEnter"
+      @mouseleave="props.onMouseLeave"
+      class="absolute p-400 bg-neutrals-200 h-[100px] w-full"
+    >
+      <slot />
+    </div>
+  </transition>
+</template>
+
+<style scoped></style>
--- src/components/layout/navbar/NavbarSecondary.vue
+++ src/components/layout/navbar/NavbarSecondary.vue
@@ -1,4 +1,5 @@
 <script setup>
+import DropdownPanel from "@/components/layout/navbar/DropdownPanel.vue";
 import { BaseButton } from "@/components/common/buttons";
 
 const mainItems = [
@@ -16,30 +17,64 @@ const secondaryItems = [
   { name: "registrations", tag: "button" },
   { name: "contact", tag: "button" },
 ];
+import { ref } from "vue";
+
+const isDropdownOpen = ref(false)
+const handleDropdownIn = (item) => {
+  console.log(isDropdownOpen.value);
+  console.log("DROPDOWN ON!");
+  isDropdownOpen.value = true
+  console.log(isDropdownOpen.value);
+  // Calculate the position for the columns
+  // based on the parent position
+  console.log(item);
+
+}
+const handleDropdownOut = () => {
+  console.log(isDropdownOpen.value);
+  console.log("DROPDOWN OFF!");
+  isDropdownOpen.value = false
+  console.log(isDropdownOpen.value);
+}
 </script>
 
 <template>
-  <div
-    class="p-400 lg:pb-0 mx-auto lg:max-w-7xl hidden md:flex items-center justify-between"
-  >
-    <ul class="flex flex-grow gap-600 justify-start items-center me-400 h-1600">
-      <li v-for="item in mainItems" :key="item" class="h-full">
-        <BaseButton
-          class="h-full uppercase"
-          as="button"
-          variant="underline"
-          size="lg"
-          >{{ $t(`navigation.${item}`) }}</BaseButton
-        >
-      </li>
-    </ul>
-    <ul class="hidden md:flex items-center space-x-400">
-      <li v-for="item in secondaryItems" :key="item">
-        <BaseButton :as="item.tag" :href="item.href" variant="gray" size="xs">{{
-          $t(`navigation.${item.name}`)
-        }}</BaseButton>
-      </li>
-    </ul>
+  <div class="relative">
+
+    <div
+      class="p-400 lg:pb-0 mx-auto lg:max-w-7xl hidden md:flex items-center justify-between"
+    >
+      <ul class="flex flex-grow gap-600 justify-start items-center me-400 h-1600">
+        <li v-for="item in mainItems" :key="item" class="h-full">
+          <BaseButton
+            class="h-full uppercase"
+            as="button"
+            variant="underline"
+            size="lg"
+            @mouseenter="handleDropdownIn(item)"
+            @mouseleave="handleDropdownOut"
+            >{{ $t(`navigation.${item}`) }}</BaseButton
+          >
+        </li>
+      </ul>
+      <ul class="hidden md:flex items-center space-x-400">
+        <li v-for="item in secondaryItems" :key="item">
+          <BaseButton :as="item.tag" :href="item.href" variant="gray" size="xs">{{
+            $t(`navigation.${item.name}`)
+          }}</BaseButton>
+        </li>
+      </ul>
+    </div>
+    <DropdownPanel :is-open="isDropdownOpen" :on-mouse-enter="handleDropdownIn" :on-mouse-leave="handleDropdownOut">
+      <div
+        class="p-400 lg:pb-0 mx-auto lg:max-w-7xl hidden md:flex items-center justify-between"
+      >
+        <ul class="flex flex-grow gap-600 justify-start items-center me-400 h-1600">
+          <p>FLAMENGO</p>
+          <p class="px-200">OCTA</p>
+        </ul>
+      </div>
+    </DropdownPanel>
   </div>
 </template>
 
